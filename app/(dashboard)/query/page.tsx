9b581213a357273d9d765eca90aeb4c3bae746e0
James Bohrman <james@getoasis.io>
2024-07-13 02:15:15 -0400

    query builder go brrr

--- app/(dashboard)/query/page.tsx
+++ app/(dashboard)/query/page.tsx
@@ -1,11 +1,9 @@
-import SQLEditor from '@/components/sqlEditor';
-
-const App = () => {
+import SQLEditor from '@/components/editor'; // Assuming SQLEditor is in the same directory
+// Then use it in your JSX
+export default function QueryEditor() {
   return (
     <div>
       <SQLEditor />
     </div>
   );
-};
-
-export default App;
+}
--- app/(dashboard)/saved-components/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ComponentsPage() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-  
