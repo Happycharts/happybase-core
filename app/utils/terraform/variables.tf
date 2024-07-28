018e11c0735b84a3b0c7ac08e79fcd6029013f06
James Bohrman <james@getoasis.io>
2024-07-27 21:43:38 -0400

    terraform

--- app/utils/terraform/variables.tf
+++ app/utils/terraform/variables.tf
@@ -19,6 +19,22 @@ variable "gcp_credentials" {
   sensitive = true
 }
 
+variable "network_name" {
+  type = string
+}
+
+variable "subnet_a" {
+  type = string
+}
+
+variable "subnet_b" {
+  type = string
+}
+
+variable "subnet_c" {
+  type = string
+}
+
 variable "region" {
   type = string
   sensitive = true
@@ -27,27 +43,6 @@ variable "region" {
 variable "cluster_name_suffix" {
   description = "A suffix to append to the default cluster name"
   default     = ""
-}
-
-variable "zones" {
-  type        = list(string)
-  description = "The zone to host the cluster in (required if is a zonal cluster)"
-}
-
-variable "network" {
-  description = "The VPC network to host the cluster in"
-}
-
-variable "subnetwork" {
-  description = "The subnetwork to host the cluster in"
-}
-
-variable "ip_range_pods" {
-  description = "The secondary ip range to use for pods"
-}
-
-variable "ip_range_services" {
-  description = "The secondary ip range to use for services"
 }
 
 variable "compute_engine_service_account" {
