48b6382afadc5d9a24eea7cf10713f8eeb3cd824
James Bohrman <james@getoasis.io>
2024-07-27 21:04:47 -0400

    terraform

--- app/utils/terraform/outputs.tf
+++ app/utils/terraform/outputs.tf
@@ -7,3 +7,23 @@ output "domain_example" {
   value       = module.project-factory.domain
   description = "The organization's domain"
 }
+
+
+output "kubernetes_endpoint" {
+  sensitive = true
+  value     = module.gke.endpoint
+}
+
+output "client_token" {
+  sensitive = true
+  value     = base64encode(data.google_client_config.default.access_token)
+}
+
+output "ca_certificate" {
+  value = module.gke.ca_certificate
+}
+
+output "service_account" {
+  description = "The default service account used for running nodes."
+  value       = module.gke.service_account
+}
