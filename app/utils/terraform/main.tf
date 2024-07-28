9f33c221edd9f05794d4164ccef7b281a08d1441
James Bohrman <james@getoasis.io>
2024-07-27 22:05:43 -0400

    terraform

--- app/utils/terraform/main.tf
+++ app/utils/terraform/main.tf
@@ -65,8 +65,8 @@ module "project-services" {
 }
 
 module "network" {
-  source  = "terraform-google-modules/network/google//examples/basic_auto_mode"
-  version = "9.1.0"
+  source  = "terraform-google-modules/network/google"
+  version = "~> 9.1"
   project_id = module.project-factory.project_id
   network_name = var.network_name
   subnets = [
